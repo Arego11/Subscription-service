c051154939334bb194d06557fe7ae91bf22792a3
Arego11 <aregar20012001@gmail.com>
2025-03-08 10:50:26 -0800

    PORT, app.js, etc added

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 import express from 'express';
+import { PORT } from './config/env.js';
+//const PORT = 3000;
 
-const app = express();
-const PORT = 3000;
+const app = express(); //initialize
 
+
+//first route, reacheable in the home page
 app.get('/', (req,res) => {
     res.send('Subscription tracker API!');
 });
-
+//makes server listen to a speciific port
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+    console.log(`Subscription tracker API is running on http://localhost:${PORT}`);
 });
 export default app;
