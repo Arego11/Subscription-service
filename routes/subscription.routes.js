15059c7c1c83b42b517a157e507991117c555be1
Arego11 <aregar20012001@gmail.com>
2025-03-13 01:01:52 -0700

    added subscription system, check info.txt for credentials

--- routes/subscription.routes.js
+++ routes/subscription.routes.js
@@ -1,18 +1,20 @@
 import { Router } from 'express';
-
+import authorize from '../middlewares/auth.middleware.js';
+import { createSubscription } from '../controllers/subscription.controller.js';
+import { getUserSubscriptions } from '../controllers/subscription.controller.js';
 const SubscriptionRouter = Router();
 
 SubscriptionRouter.get('/', (req,res) => res.send({title: 'GET all subscriptions'}));
 
 SubscriptionRouter.get('/:id', (req,res) => res.send({title: 'GET subscriptions details'}));
 
-SubscriptionRouter.post('/', (req,res) => res.send({title: 'CREATE subscriptions'}));
+SubscriptionRouter.post('/', authorize, createSubscription);
 
 SubscriptionRouter.put('/:id', (req,res) => res.send({title: 'UPDATE subscriptions'}));
 
 SubscriptionRouter.delete('/:id', (req,res) => res.send({title: 'DELETE all subscriptions'}));
 
-SubscriptionRouter.get('/user/:id', (req,res) => res.send({title: 'GET all user subscriptions'}));
+SubscriptionRouter.get('/user/:id', authorize, getUserSubscriptions);
 
 SubscriptionRouter.put('/:id/cancel', (req,res) => res.send({title: 'CANCEL subscriptions'}));
 
